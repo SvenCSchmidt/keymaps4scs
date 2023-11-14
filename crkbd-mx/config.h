f62251f1be171ae3c2a1cacfb4d7c441d53f0fa2
Sven Schmidt <68299849+SvenCSchmidt@users.noreply.github.com>
2023-11-14 13:28:31 +0100

    Changed Keymaps

--- crkbd-mx/config.h
+++ crkbd-mx/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
 #define VIAL_UNLOCK_COMBO_COLS {0, 1}
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define TAPPING_TERM 180
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define TAPPING_TERM 200
 
 //#define USE_MATRIX_I2C
 #ifdef KEYBOARD_crkbd_rev1_legacy
@@ -49,6 +49,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_HUE_STEP  10
 #    define RGBLIGHT_SAT_STEP  17
 #    define RGBLIGHT_VAL_STEP  17
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#    define RGB_MATRIX_KEYPRESSES
+#    define BACKLIGHT_BREATHING
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
--- crkbd-mx/rules.mk
+++ crkbd-mx/rules.mk
@@ -2,12 +2,15 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
-RGBLIGHT_ENABLE     = yes
-RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
-MOUSEKEY_ENABLE     = no
-OLED_ENABLE         = no
-OLED_DRIVER         = SSD1306
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+BACKLIGHT_ENABLE	= yes
+MOUSEKEY_ENABLE     = yes
+OLED_ENABLE         = yes
+#OLED_DRIVER         = SSD1306
 EXTRAKEY_ENABLE     = yes
-COMBO_ENABLE        = no
+TAP_DANCE_ENABLE	= yes
+COMBO_ENABLE        = yes
+CONVERT_TO=helios
 
 QMK_SETTINGS        = no
